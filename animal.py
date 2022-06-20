1278a7d736f91b7b94c3c6cd666da0168ae5a885
Dennis <lamden@oregonstate.edu>
2022-06-20 10:31:11 -0600

    Created program that prints the user input of their favorite animal.

---
+++ animal.py
@@ -0,0 +1,8 @@
+#Author: Dennis Lam
+#GitHub username: dennislam4
+#Date: 6/20/2022
+#Description: Asks the user for their favorite animal and then
+#             prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
